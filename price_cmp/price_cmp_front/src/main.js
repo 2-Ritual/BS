2341f0fcf7e7cfac44a8ba3ce23f36588c88afc5
2_Ritual <3011371796@qq.com>
2024-10-23 09:47:47 +0800

    登陆界面

--- price_cmp/price_cmp_front/src/main.js
+++ price_cmp/price_cmp_front/src/main.js
@@ -1,17 +1,17 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import login_page from './components/login_page.vue';
-import register_page from './components/register_page.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import axios from 'axios'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import App from './App.vue'
+import router from './router'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-const routes = [
-    { path: '/login', component: login_page },
-    { path: '/register', component: register_page }
-];
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-});
-
-createApp(App).use(router).mount('#app');
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+axios.defaults.baseURL = 'http://localhost:8000';
+app.use(createPinia())
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
